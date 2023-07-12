f085aa3f52afc2f2a0f8563804488ab62db47468
Nitu Dubey <nitutiwari01@gmail.com>
2023-07-11 23:27:12 -0400

    submitted

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => setName(e.target.value);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <label>Enter your name:</label>
+      <br />
+      <input type="text" onChange={handleChange}></input>
+      <p>Hello {name}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
